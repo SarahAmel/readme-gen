cff9918cfed5ccddd477d62fff7554e94ec7a0bf
sarah attar <sarahattar@sarahs-MacBook-Pro.local>
2022-06-01 16:14:54 -0400

    added code to the index.js file in the root of the folder structure as well as the markdown file inside of the utils folder

--- index.js
+++ index.js
@@ -1,13 +1,123 @@
+
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fileName = "README.md"
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github name?',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+      type: 'input',
+      name: 'location',
+      message: 'What is your email adress?',
+    },
 
-// Function call to initialize app
+    {
+      type: 'input',
+      name: 'project desciption',
+      message: 'Tell me about the project?',
+    },
+
+    {
+      type: 'input',
+      name: 'project examples ',
+      message: 'Does your project have any visual examples',
+    },
+
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What license does your project have?',
+    },
+
+    {
+      type: 'input',
+      name: 'comand',
+      message: 'What command should I run to install dependencies?',
+    },
+
+
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run for tests?',
+      },
+
+
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who contributed to the repo?',
+      },
+
+
+      {
+        type: 'input',
+        name: 'important',
+        message: 'What does your user need to know about using the repository?',
+      },
+  ]
+
+  
+const init = () => {
+
+inquirer.prompt(questions)
+.then((data) =>  fs.writeFile ("READ.md", generateMarkdown, (data)))
+
+.then((data)=> console.log("Good job kid!"))
+
+.catch((err) => console.error(err));
+
+}
+
+
 init();
+
+
+  // .then((answers) => {
+  //   const htmlPageContent = generateHTML(answers);
+
+    
+    
+    
+    
+    
+  //   fs.writeFile('index.html', htmlPageContent, (err) =>
+  //     err ? console.log(err) : console.log('Successfully created index.html!')
+  //   );
+  // });
+
+
+
+
+  // function writeToFile(fileName, data) {}
+
+  // function init() {}
+  
+  
+  // init();
+
+
+  // Function call to initialize app
+// Function call to initialize app
+// TODO: Create a function to initialize app
+// TODO: Create a function to write README file
+
+  // What's your GitHub username?
+// What is your email address?
+// What is your project title?
+// Please write a short project description.
+// Do you have a link to a picture or video for the application?
+// What kind of license does your project have?
+// What command should I run to install dependencies?
+// What command should be run for tests?
+// What does your user need to know about using the repository?
+// Who contributed to the repo?
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,48 +4,265 @@ function renderLicenseBadge(license) {
   if (license !== "None"){
     return `[License]($(license))`
   }
+return "";
 
- return "";
+
+if (license !== "MIT"){
+ 
+ 
+  return `[License:MIT] [((https://img.shields.io/badge/license-MIT-green)(https://choosealicense.com/licenses/mit/)`
 
 
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-if (license !== "None"){
-  return `/n* [Licence](#) /n`
-}
-
 
 return "";
 
+if (license !== "Apache"){
+ 
+ 
+  return `[License] (https://img.shields.io/badge/license-Apache-green) (https://opensource.org/licenses/Apache-2.0)`
+
 }
+
+return "";
+
+
+if (license !== "Affero General Public"){
+  return `[License] (https://img.shields.io/badge/license-GNU%20Affero%20General%20Public-green)(https://choosealicense.com/licenses/agpl-3.0/)`
+}
+
+
+return " "
+
+
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {
+// if (license !== "None"){
+//   return `/n* [Licence](#) /n`
+// }
+
+
+// return "";
+
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+// function renderLicenseSection(license) {
 
 
-  if (license !== "None"){
-    return `## License
+//   if (license !== "None"){
+//     return `## License
 
-    This project is licensed under $(put the name of the license).`;
-  }
+//     This project is licensed under $(put the name of the license).`;
+//   }
   
   
-  return "";
+//   return "";
   
   
 
-}
+// }
 
 // TODO: Create a function to generate markdown for READMEnp
 // reutn the whole reame the title who made the project 
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ##Title   
+  # ${data.title}
+  
+
+ ##Table of contents 
+ 
+ [Description](#Description)
+
+ [Navagaition](#Navigation)
+
+ [Screenshit](#Screenshot)
+
+ [Video](#Video)
+
+ [License](#License)
+
+ [Links](#Links)
+
+ [Refrences](#Refrences)
+
+
+
+ ##Description 
+  ${data.description}
+
+
+  ##Navigation
+  ${data.Navigation}
+
+ ##Screenshot
+ [Project Screenshot](${data.screenshot})
+
+
+ ## Screenshot
+ [Project Screenshot](${data.screenshot})
+
+ #video
+ [Video Link](${data.videolink})
+
+
+ ## License
+ ${renderLicenseBadge(data.license)}
+
+
+
+ ## Links
+
+[Github Link](${data.github})
+
+[Portfolio Link](${data.portfolio})
+  
+[Email](${data.email})
+  
+##Refrences
+(${data.refrences})
+
 
 `;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = generateMarkdown;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use as ref for markdown 
+// const generateHTML = ({ name, location, github, linkedin }) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${name}</h1>
+//     <p class="lead">I am from ${location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${github}</li>
+//       <li class="list-group-item">LinkedIn: ${linkedin}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`
